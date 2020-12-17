530ea9004233bc659c59d0d30ca4bd25fd31e4cc
PierMario <piermario.salis@aol.com>
2020-12-17 12:45:48 +0100

    Deny Empty Tasks
    E' possibile inserire tasks con minimo 6 caratteri.

--- main.js
+++ main.js
@@ -1,9 +1,3 @@
-
-
-// Se non ci sono piu task nella lista, mostrate un messaggio tipo "Nulla da fare"
-// Quando l'utente inserisce una task ha due modi per salvarla: o preme il pulsante add o preme il taso Enter della tastiera.
-// Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
-
 // La nostra todo list avrá alcune tasks di default predefinite
 let startToDo = new Vue ({
     el: "#start",
@@ -27,9 +21,12 @@ let startToDo = new Vue ({
     // L'utente puó inserire nuove tasks
     methods: {
         AddNewTask: function(){
+            // Attenzione: l'utente non deve inserire tasks vuote ma almeno un tot di caratteri.
+            if (this.newTask.length >= 6) {
             this.tasks.push({
                 task: this.newTask
             });
+        }
         },
         // Cliccando sulla "X" l'utente puó cancellare una task
         deleteTask(index) {
